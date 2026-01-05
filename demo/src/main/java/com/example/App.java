dba06addc06e25ac8dadb03bef62d62da273800a
Ivan Carrasco <alu.ivan.carrasco@mataro.epiaedu.cat>
2026-01-05 15:57:02 +0100

    Segon Commit, Estetica del menú

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
 
 public class App {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         // Opció 1: deixar que el client llegeixi la clau de GEMINI_API_KEY / GOOGLE_API_KEY
         // Client client = new Client();
-
+        boolean exit = false;
         // Opció 2: passar explícitament l'API key (també la llegeixo de l'entorn)
         String apiKey = System.getenv("GEMINI_API_KEY");// Reemplaça-ho amb la teva clau d'API real
         if (apiKey == null || apiKey.isEmpty()) {
@@ -15,6 +21,39 @@ public class App {
             System.err.println("Si us plau, exporta GEMINI_API_KEY amb la teva API key de Gemini.");
             return;
         }
+        do {
+            System.out.println("------------------------------");
+            System.out.println(" Generador de Sets de Dades");
+            System.out.println("------------------------------");
+            System.out.println("1. Generar un nou set de dades");
+            System.out.println("2. Visualitzar sets de dades");
+            System.out.println("3. Esborrar sets de dades");
+            System.out.println("4. Sortir");
+            System.out.print("Opció: ");
+
+            String entrada = sc.nextLine();
+
+            switch (entrada) {
+                case "1":
+                    // Generar sets
+                    break;
+
+                case "2":
+                    // Visualitzar sets
+                    break;
+
+                case "3":
+                    // Esborrar sets
+                    break;
+
+                case "4":
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("Opció incorrecta");
+            }
+        } while (!exit);
 
         Client client = Client.builder()
                 .apiKey(apiKey)  // usem l'API key del Gemini Developer API
